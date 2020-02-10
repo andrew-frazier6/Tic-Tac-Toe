2f2d7235ea6aa8e5d2a6efd58d5f65190c3ea834
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-10 14:32:43 -0500

    finished reset function

--- function.js
+++ function.js
@@ -1,16 +1,34 @@
 let box = document.querySelectorAll(".box");
 let color = "red";
+let totalClicks = 0;
+let button = document.querySelector("button");
+button.addEventListener("click", reset);
 
-for (let i = 0; i < box.length; i++) {
-  box[i].addEventListener("click", changeColor);
-  function changeColor() {
-    box[i].style.backgroundColor = color;
-    if (color == "red") {
-      color = "blue";
-      box[i].removeEventListener("click", changeColor);
-    } else {
-      color = "red";
-      box[i].removeEventListener("click", changeColor);
-    }
+function resetGame() {
+  for (let i = 0; i < box.length; i++) {
+    box[i].addEventListener("click", changeColor);
+  }
+  totalClicks = 0;
+}
+resetGame();
+// after 9 something would happen
+
+function changeColor(e) {
+  console.dir(e);
+  e.target.style.backgroundColor = color;
+  totalClicks += 1;
+  if (color == "red") {
+    color = "blue";
+    e.target.removeEventListener("click", changeColor);
+  } else {
+    color = "red";
+    e.target.removeEventListener("click", changeColor);
+  }
+}
+
+function reset() {
+  if (totalClicks === 9) {
+    for (let i = 0; i < box.length; i++) box[i].style.backgroundColor = "white";
+    resetGame();
   }
 }
